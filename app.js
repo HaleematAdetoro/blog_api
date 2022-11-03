a2828339b7fc539c2873b901431d3353898ed8e6
Haleemat Adetoro <haleematadetoro24@gmail.com>
2022-11-03 23:10:15 +0100

    refactor app.js for testing

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const passport = require('passport');
+const app = require('./index')
 
-const AuthRouter = require('./routes/auth')
-
-const app = express();
 const PORT = process.env.PORT || 5000
 
 require('./database/db').connectToMongoDB()
 require('dotenv').config()
-
-require('./middleware/auth')
-
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-
-app.use('/', AuthRouter);
-
-
-app.get('/', (req, res) => {
-    res.send('home')
-})
 
 
 
