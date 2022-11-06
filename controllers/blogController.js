3933a5ae7953c8f5534f016bcd683a44448af301
Haleemat Adetoro <haleematadetoro24@gmail.com>
2022-11-06 17:38:23 +0100

    fix blog controllers

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,83 +1,97 @@
 
 const BlogModel = require('../models/blogModel');
 
-const getAllUsersBlogs = async (req, res) => {
-    const blogs = await BlogModel
-    .select({ title: 1})
-    .find({ state: 'published' })
-    .populate('author', { username: 1 })
 
-    return res.json({ status: true, data: blogs })
+const getAllUserBlogs = async (req, res) => {
+    try {
+        const  currentUserId  = req.user._id;
+        
+        const blog = await BlogModel.find({ author: currentUserId})
+            .sort({ state: 'published' })
+        
+        
+        return res.json({ status: true, data: blog })
+    } catch (error) {
+        return(error)
+    }
+   
 };
 
 const getAllPublishedBlogs = async (req, res) => {
-    const { query } =req;
+    try {
+        const { query } =req;
 
-    const {
-        author,
-        title,
-        tags,
-        order = 'asc',
-        order_by = 'read_time',
-        page = 1,
-        per_page = 20
-    } = query;
-
-    const findQuery ={};
-
-    if (author) {
-        findQuery.author = author;
-    }
-
-    if (title) {
-        findQuery.title = title;
-    }
-
-    if (tags) {
-        findQuery.tags = tags;
-    }
+        const {
+            author,
+            title,
+            tags,
+            order = 'asc',
+            order_by = 'read_time',
+            per_page = 20
+        } = query;
     
-    const sortQuery = {};
-
-    const sortAttributes = order_by.split(',')
-
-    for (attributes of sortAttributes) {
-        if (order === 'asc' && order_by) {
-            sortQuery[attribute] = 1
+        const findQuery ={};
+        if (author) {
+            findQuery.author = author;
         }
     
-        if (order === 'desc' && order_by) {
-            sortQuery[attribute] = -1
+        if (title) {
+            findQuery.title = title;
         }
+    
+        if (tags) {
+            findQuery.tags = tags;
+        }
+        
+        const sortQuery = {};
+    
+        const sortAttributes = order_by.split(',')
+    
+        for (attribute of sortAttributes) {
+            if (order === 'asc' && order_by) {
+                sortQuery[attribute] = 1
+            }
+        
+            if (order === 'desc' && order_by) {
+                sortQuery[attribute] = -1
+            }
+        }
+    
+        const blogs = await BlogModel
+        .find({ state: 'published'})
+        .find(findQuery)
+        .sort(sortQuery)
+        .limit(per_page)
+    
+        return res.status(200).json({ status: true, blogs})
+
+    } catch (error) {
+        return(error)
     }
-
-    const blogs = await BlogModel
-    .find(findQuery)
-    .sort(sortQuery)
-    .skip(page)
-    .limit(per_page)
-
-    return res.status(200).json({ status: true, blogs})
 
 }
 
 const getPublishedBlog = async (req, res) => {
-    const { id } = req.params;
-    const blog = await BlogModel.findById(id)
-        .populate('author', { username: 1 })
+    try {
+        const { blogId } = req.params;
+        const blog = await BlogModel.findById(blogId)
+            .populate('author', {username: 1})
 
-    if (!blog) {
-        return res.status(404).json({ status: false, blog: null })
+        if (!blog) {
+            return res.status(404).json({ status: false, blog: null })
+        }
+
+        if (blog.state !== 'published') {
+            return res.status(404).json({ status: false, blog: null })
+        }
+    
+        blog.read_count += 1
+        await blog.save()
+
+        return res.json({ status: true, blog })
+    } catch (error) {
+        return(error)
     }
-
-    if (blog.state !== 'published') {
-        return res.status(404).json({ status: false, blog: null })
-    }
-
-    blog.read_count += 1
-    await blog.save()
-
-    return res.json({ status: true, blog })
 }
 
 const createNewBlog = async (req, res) => {
@@ -105,51 +119,102 @@ const createNewBlog = async (req, res) => {
             data: createdblog,
         })
     } catch (error) {
-        console.log(error)
         return (error)
     }
     
 };
 
-const updateOneBlog = async (req, res) => {
-    const { id } = req.params;
-    const { state } = req.body;
+const updateBlogState = async (req, res) => {
+    try {
+        const { blogId } = req.params;
+        const { state } = req.body;
 
-    const blog = await BlogModel.findById(id)
+        const blog = await BlogModel.findById(blogId)
 
-    if (!blog) {
-        return res.status(404).json({ status: false, blog: null })
+        if (!blog) {
+            return res.status(404).json({ status: false, blog: null })
+        }
+
+        if (!blog.author.equals(req.user.id)) {
+            return res.status(403).json({
+                status: false,
+                message: "You can't edit blog"
+        })
+        }
+
+        if (state === blog.state) {
+            return res.status(422).json({ status: false, blog: null, message: 'Invalid operation' })
+        }
+
+        blog.state = state;
+
+        await blog.save()
+
+        return res.json({ status: true, blog })
+    } catch (error) {
+        return(error)
     }
-
-    if (state === blog.state) {
-        return res.status(422).json({ status: false, blog: null, message: 'Invalid operation' })
-    }
-
-    blog.state = state;
-
-    await blog.save()
-
-    return res.json({ status: true, blog })
 };
 
 const updateBlog = async (req, res) => {
+    try{
+        const  blogId   = req.params.blogId
+        const { title, description, body, tags } = req.body
 
+        const blog = await BlogModel.findById(blogId);
+        
+        if (!blog) {
+            return res.status(404).json({ status: false, blog: null })
+        }
+        if (!blog.author.equals(req.user._id)) {
+            return res.status(403).json({
+                status: false,
+                message: "You can't edit blog"
+            })
+        }
+
+        blog.title = title || blog.title,
+        blog.description = description || blog.description;
+        blog.body = body || blog.body;
+        blog.tags = tags || blog.tags;
+        
+        await blog.save();
+        res.status(200).json({ status: true, blog })
+        
+    } catch (error) {
+        return(error);
+    }
 }
 
 const deleteOneBlog = async (req, res) => {
-    const { id } = req.params;
+    try {
+        const { blogId } = req.params;
 
-    const blog = await BlogModel.deleteOne({ _id: id})
+        const blog = await BlogModel.findById(blogId)
 
-    return res.json({ status: true, blog })
+        if (!blog.author.equals(req.user.id)) {
+            return res.status(403).json({
+            status: false,
+            message: "You can't delete blog"
+            })
+        }
+
+        await BlogModel.deleteOne({ _id: blogId})
+
+        return res.json({ status: true,
+            message: "Blog delete succesfully"})
+    } catch (error) {
+        return(error)
+    }
 }
 
 module.exports = {
-    getAllUsersBlogs,
+    getAllUserBlogs,
     getAllPublishedBlogs,
     getPublishedBlog,
     createNewBlog,
-    updateOneBlog,
+    updateBlogState,
+    updateBlog,
     deleteOneBlog
 }
 
